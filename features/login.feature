a5f96fce5561393f75d82a706e0eeef4aaa0d0c6
Camilo Padilla <camilo.padilla@ucb.edu.bo>
2025-05-28 18:56:48 -0400

    Arreglo login

--- features/login.feature
+++ features/login.feature
@@ -1,7 +1,19 @@
-Feature: Login en SauceDemo
+Feature: Login
+  In order to acceder a mi cuenta y comprar productos
+  As an usuario registrado
+  I want to iniciar sesión exitosamente
 
-  Scenario: Iniciar sesión exitosamente con credenciales válidas
-    Given que estoy en la página de inicio
-    When ingreso el usuario "standard_user" y la contraseña "secret_sauce"
-    And hago clic en el botón de login
-    Then debería ver la página de productos
+  @this
+  Scenario: Login exitoso
+    Given que estoy en la página de login
+    And ingreso el usuario "standard_user" y la contraseña "secret_sauce"
+    When hago clic en el botón Login
+    Then debería ser redirigido a la página de productos
+    And debería ver el título "Products"
+
+  @only
+  Scenario: Login con credenciales incorrectas
+    Given que estoy en la página de login
+    And ingreso el usuario "standard_user" y una contraseña incorrecta "secretUser"
+    When hago clic en el botón Login
+    Then debería ver un mensaje de error que diga "Username and password do not match any user in this service"
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,16 +1,37 @@
-Given('que estoy en la página de inicio') do
-  visit('/')
+Given('que estoy en la página de login') do
+    page.driver.browser.manage.window.maximize
+    visit('/')
 end
 
-When('ingreso el usuario {string} y la contraseña {string}') do |usuario, contrasena|
-  fill_in('user-name', with: usuario)
-  fill_in('password', with: contrasena)
+Given('ingreso el usuario {string} y la contraseña {string}') do |user, password|
+    fill_in 'user-name', :with => user
+    fill_in 'password', :with => password
 end
 
-When('hago clic en el botón de login') do
-  click_button('login-button')
+When('hago clic en el botón Login') do
+    click_button("login-button") 
 end
 
-Then('debería ver la página de productos') do
-  expect(page).to have_content('Products')
+Then('debería ser redirigido a la página de productos') do
+    expect(page).to have_selector('#header_container > div.header_secondary_container > span', text: 'Products')
 end
+
+Then('debería ver el título {string}') do |expected_title|
+    actual_title = find(:css,'#header_container > div.header_secondary_container > span').text 
+    if actual_title == expected_title
+        puts "El título es correcto: #{actual_title}"
+    end
+end
+
+Given('ingreso el usuario {string} y una contraseña incorrecta {string}') do |user,passwordFail|
+    fill_in 'user-name', :with => user
+    fill_in 'password', :with => passwordFail
+end
+
+Then('debería ver un mensaje de error que diga {string}') do |errorMessage|
+    error = find(:css, '#login_button_container > div > form > div.error-message-container.error > h3').text
+    if error == errorMessage
+        puts "El mensaje de error es correcto: #{error}"
+    end
+end
+
